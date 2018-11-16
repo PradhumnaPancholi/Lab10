dd312536a4d45ac2ade1f670d0e7ded5093d215c
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-11-16 08:58:16 -0500

    Book obj with instances

---
+++ src/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    public String title, author;
+    public int price;
+
+    public Book(String title, String author, int price)
+    {
+        setTitle(title);
+        setAuthor(author);
+        setPrice(price);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Book book1 = new Book("Creative Title 1", "Jaret Wright", 78  );
+        Book book2 = new Book("Creative Title 2", "Jaret Wright", 78  );
+        Book book3 = new Book("Creative Title 3", "Jaret Wright", 78  );
+        Book book4 = new Book("Creative Title 4", "Jaret Wright", 78  );
+
+    }
+}
