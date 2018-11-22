48f6f1f4796bfae9ce61a38d2d89a21b9676aa37
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-11-22 17:51:32 -0500

    added book objects treeset ts1  and  printed them out

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        Book book1 = new Book("Creative Title E1", "Jaret Wright", 78  );
-        Book book2 = new Book("Creative Title E2", "Jaret Wright", 56  );
-        Book book3 = new Book("Creative Title E3", "Jaret Wright", 86  );
-        Book book4 = new Book("Creative Title E4", "Jaret Wright", 115  );
+        Book book1 = new Book("How to Write Creative Titles E1", "Jaret Wright", 78  );
+        Book book2 = new Book("How to Write Creative Titles E2", "Jaret Wright", 56  );
+        Book book3 = new Book("How to Write Creative Titles E3", "Jaret Wright", 86  );
+        Book book4 = new Book("How to Write Creative Titles E4", "Jaret Wright", 115  );
 
+        //Data structure to filter duplicates//
+        TreeSet<Object> ts1= new TreeSet<>();
+
+        //adding book objects to treeset//
+        ts1.add(book1);
+        ts1.add(book2);
+        ts1.add(book3);
+        ts1.add(book4);
+
+        System.out.println(ts1);
     }
 }
